c8287756bd7b199e788ed854bdad31bb9a7079d4
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-06-16 05:04:05 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@ background-color:white;
         
 <header class="justify-content-center flex-column">
 
-<nav class="navbar nav1 navbar-expand-lg" style="height: 70px">
+<nav class="navbar nav1  bg-dark navbar-expand-lg" style="height: 70px">
 <div class="w-25">
         <a href="/" class="navbar-brand">
             <img src="{{asset('image/project2.jpg.png')}}" class="img-fluid w-50 h-50" style="margin-left:185px">
