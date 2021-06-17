df0c079fa75e6978bc9ea8bbce6035d22b927e59
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-06-17 09:30:51 -0700

    Update blog.blade.php

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+    
+    <style>
+       *{
+            margin:0;
+            padding:0;
+            font-family:sans-serif;
+        }
+         header{
+                background: url("{{asset('image/g3.jpg')}}")
+                no-repeat center;
+                height:500px;
+                background-size: cover;
+            }
+            .div4{
+            height: auto;
+                background-color: #003345;
+            }
+        </style>
+        </head>
+        <body>
+            <div class="ml-5 py-3">
+            <button class="btn text-danger ">Sign in</button>
+            <button class="btn  btn-outline-danger rounded-pill py-2 px-4">Get Started</button>
+            </div>
+            <header class="">
+
+            <div class="container container-sm">
+<div class="row d-flex justify-content-between align-items-center flex-column text-white">
+<div class="col mt-5">
+<img src="{{asset('image/project2.jpg.png')}}" alt="" class="mt-2 img-fluid" style="height: 100px">
+<h1 class= "text-white font-weight-bold">
+ Profit Bank Associate
+    </h1>
+    <h5 class= "text-white font-weight-bold py-2">
+    Quality |  Trust  | Proximity
+     </h5>
+   
+ </div>
+</div>
+</div>
+            </header>
+            <div class="container">
+            <div class="row mx-auto">
+            <div class="col-12 col-lg-7 py-3 text-black">
+            <a href="" class=" mr-2 text-black">AMAS</a>
+                        <a href="" class=" mr-2 text-black">Articals</a>
+                        <a href="" class=" mr-2 text-black">Yearly Recaps</a>
+                        <a href="" class="text-black">| our Website</a>
+            </div>
+            <div class="col-12 col-lg-5 py-3  ">
+            <a href="" class="ml-5 mt-4">
+            <i class="fab fa-search " style="font-size: 25px"></i>
+        </a>
+            <a href="" class="ml-5 mt-4">
+            <i class="fab fa-telegram-plane text-black " style="font-size: 25px"></i>
+        </a>
+
+        <a href="" class="ml-4 mt-4">
+            <i class="fab fa-discord text-black " style="font-size: 25px"></i>
+        </a>
+
+        <a href="" class="ml-4 mt-4">
+            <i class="fab fa-facebook-f text-black" style="font-size: 25px"></i>
+        </a>
+
+        <a href="" class="ml-4 mt-4">
+            <i class="fab fa-instagram mr-2 text-black " style="font-size: 25px"></i>
+        </a>
+        <button class="btn btn-outline-danger rounded-pill ">Follow</button>
+            </div>
+            </div>
+            </div>
+            <hr>
+            <div class="container container-sm mt-5">
+            <div class="row mx-auto">
+            <div class="col-12 col-lg-7">
+            <img src="{{asset('image/a3.jpg')}}" alt="" class="img-fluid mb-5">
+            </div>
+            <div class="col-12 col-lg-5 ">
+            <h1>TwoTwo-Telegram</h1>
+            <h3>AMA-May 03</h3>
+            <p>        GAINS stands for Group Action Is Never Small, 
+            embodying the pooling and  Group Action Is Never Small </p>
+            <div class="row">
+            <div class="col-2 ">
+            <img src="{{asset('image/images.png')}}" alt="" class="img-fluid rounded-circle" height="100px" width="100px">
+            </div>
+            <div class="col-10">
+            <h4 class="text-danger">GAIN Associates</h4>
+            <p>Jun 15 . 10 min read</p>
+            
+            </div>
+            </div>
+            
+            </div>
+            </div>
+            </div>
+            <div class="container container-sm">
+            <div class="row mx-auto">
+            <div class="col-12 col-lg-4">
+            <img src="{{asset('image/a3.jpg')}}" alt="" class="img-fluid mb-2">
+            <h4>ZKChas-Telegram </h1>
+            <h5>AMA-May 02</h3>
+            <div class="row">
+            <div class="col-2 ">
+            <img src="{{asset('image/images.png')}}" alt="" class="img-fluid rounded-circle" height="100px" width="100px">
+            </div>
+            <div class="col-10">
+            <h5 class="text-danger">GAIN Associates</h4>
+            <p>Jun 30 . 10 min read</p>
+            
+            </div>
+            </div>
+            </div>
+            <div class="col-12 col-lg-4">
+            <img src="{{asset('image/a3.jpg')}}" alt="" class="img-fluid mb-2">
+            <h4>NFTIFY-Telegram</h1>
+            <h5>AMA-May 30</h3>
+            <div class="row">
+            <div class="col-2 ">
+            <img src="{{asset('image/images.png')}}" alt="" class="img-fluid rounded-circle" height="100px" width="100px">
+            </div>
+            <div class="col-10">
+            <h5 class="text-danger">GAIN Associates</h4>
+            <p>may 15 . 10 min read</p>
+            
+            </div>
+            </div>
+            
+            </div>
+            <div class="col-12 col-lg-4">
+            <img src="{{asset('image/a3.jpg')}}" alt="" class="img-fluid mb-2">
+            <h4>TwoTwo-Telegram</h1>
+            <h5>AMA-May 03</h3>
+            <div class="row">
+            <div class="col-2 ">
+            <img src="{{asset('image/images.png')}}" alt="" class="img-fluid rounded-circle" height="100px" width="100px">
+            </div>
+            <div class="col-10">
+            <h5 class="text-danger">GAIN Associates</h4>
+            <p>Jun 15 . 10 min read</p>
+            
+            </div>
+            </div>
+            
+            </div>
+            
+            </div>
+            
+            </div>
+            <div class="container container-sm">
+            <div class="row mx-auto">
+            <div class="col-12 col-lg-4">
+            <img src="{{asset('image/a3.jpg')}}" alt="" class="img-fluid mb-2">
+            <h4>Trias-Telegram</h1>
+            <h5>AMA-May 03</h3>
+            <div class="row">
+            <div class="col-2 ">
+            <img src="{{asset('image/images.png')}}" alt="" class="img-fluid rounded-circle" height="100px" width="100px">
+            </div>
+            <div class="col-10">
+            <h5 class="text-danger">GAIN Associates</h4>
+            <p>Jun 15 . 10 min read</p>
+            
+            </div>
+            </div>
+            </div>
+            <div class="col-12 col-lg-4">
+            <img src="{{asset('image/a3.jpg')}}" alt="" class="img-fluid mb-2">
+            <h4>Altura-Telegram</h1>
+            <h5>AMA-May 23</h3>
+            <div class="row">
+            <div class="col-2 ">
+            <img src="{{asset('image/images.png')}}" alt="" class="img-fluid rounded-circle" height="100px" width="100px">
+            </div>
+            <div class="col-10">
+            <h5 class="text-danger">GAIN Associates</h4>
+            <p>Jun 15 . 10 min read</p>
+            
+            </div>
+            </div>
+            
+            </div>
+            <div class="col-12 col-lg-4">
+            <img src="{{asset('image/a3.jpg')}}" alt="" class="img-fluid mb-2">
+            <h4>Unore-Telegram</h1>
+            <h5>AMA-April 03</h3>
+            <div class="row">
+            <div class="col-2 ">
+            <img src="{{asset('image/images.png')}}" alt="" class="img-fluid rounded-circle" height="100px" width="100px">
+            </div>
+            <div class="col-10">
+            <h5 class="text-danger">GAIN Associates</h4>
+            <p>April 15 . 10 min read</p>
+            
+            </div>
+            </div>
+            
+            </div>
+            
+            </div>
+            
+            </div>
+            <div class="div4">
+                        <div class="container container-sm">
+                        <div class="row mx-auto">
+                        <div class="col-8 col-sm-12 col-md py-3">
+                        <a href="" class="text-white mr-2">About</a>
+                        <a href="" class="text-white mr-2">Term & Condition</a>
+                        <a href="" class="text-white mr-2">FAQ</a>
+                        <a href="" class="text-white">Contact</a>
+                        </div>
+                        <div class="col-4 col-sm-12 col-md py-3 ">
+                        <h6 class="text-white">&copy;2021 ProfitBank</h3>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+        </body>
+        </html>
