1072845e0ab515f77a996236d43bf2ac3f8e7a9e
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-06-18 08:54:58 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="js/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
     
         <style>
@@ -156,7 +158,10 @@ background-color:white;
         </ul>
     </div>
 </nav>
-<div class="container " style="margin-top:80px">
+<div data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine">
+     <div class="container " style="margin-top:80px">
 <div class="row d-flex justify-content-between align-items-center d-column text-white">
 <div class="col">
 <h1 class= "text-white font-weight-bold p-4">
@@ -171,6 +176,8 @@ background-color:white;
 </div>
 </div>
 
+</div>
+
 
 </header>
 <div class="container-fluid">
@@ -183,6 +190,9 @@ background-color:white;
     </div>
     <div class="container container-sm mt-5">
     <h1 class="text-center mb-5">Profit Bank Ranking System</h1>
+    <div data-aos="fade-up"
+     data-aos-duration="3000">
+
     <div class="row mx-auto">
     <div class="col-12  col-lg-3  mb-4" >
     <div class="card p-3 rank">
@@ -245,7 +255,7 @@ background-color:white;
   
     </div>
     </div>
-    
+    </div>
     </div>
     
     <div class="div1">
@@ -253,14 +263,19 @@ background-color:white;
 
     <h1 class="text-center mt-5 text-white py-3" >Gains Offers you</h1>
         <div class="row">
-        <div class="col-lg col-sm-12 col-md-12 bg-white mr-2 text-center bor" style="height: 450px; width:100%; margin-top: 100px;">
+        <div data-aos="fade-up"
+     data-aos-duration="3000">
+       <div class="col-lg col-sm-12 col-md-12 bg-white mr-2 text-center bor" style="height: 450px; width:100%; margin-top: 100px;">
     <img src="{{asset('image/images.png')}}" alt="" class="img-fluid text-center w-25 h-25 mt-5">
         <h5 class="text-center text-dark mt-5">Insightes</h5>
         <p class="text-center text-dark">
             Find new, fun and interactive ways to learn more about crypto projects and blockchain thanks to our regular Telegram events, with the opportunity to earn rewards.
         </p>
         </div>
-
+</div>
+      
+<div data-aos="fade-up"
+     data-aos-duration="3000">
         <div class="col-lg col-sm-12 col-md-12 bg-white mr-2 text-center bor" style="height: 450px; width:100%; margin-top: 100px">
         <img src="{{asset('image/c1.jpg')}}" alt="" class="img-fluid text-center w-25 h-25 mt-5" >
         <h5 class="text-center text-dark mt-5">Events</h5>
@@ -268,13 +283,18 @@ background-color:white;
             Being in crypto on your own can be overwhelming and risky. Exchange with experienced and well connected crypto enthusiasts about news, ideas, investment strategies and more.
          </p>
 </div>
-<div class="col-lg col-sm-12 col-md-12 bg-white bor text-center mb-5" style="height: 450px; width:100%; margin-top: 100px">
+</div>
+     <div data-aos="fade-up"
+     data-aos-duration="3000">
+     <div class="col-lg col-sm-12 col-md-12 bg-white bor text-center mb-5" style="height: 450px; width:100%; margin-top: 100px">
         <img src="{{asset('image/handship.jpg')}}" alt="" class="img-fluid text-center w-25 h-25 mt-5">
         <h5 class="text-center text-dark mt-5">Collabrition</h5>
         <p class="text-center text-dark">
             Don’t get lost in this continuous flow of complex information thanks to our synthetic daily reports. Our reviews and articles bring you to a deeper understanding of how crypto and blockchain work.
         </p>
 </div>
+</div>
+
 </div>
     </div>
     </div>
@@ -357,7 +377,9 @@ background-color:white;
 </div>
 
 <div class="container" style="margin-top: 100px">
-    <h1 class="text-center mt-5 mb-5">Profit Company Clients</h1>
+<div data-aos="fade-up"
+     data-aos-duration="3000">
+     <h1 class="text-center mt-5 mb-5">Profit Company Clients</h1>
     <div class="row">
         <div class="col btn">
             <img src="{{asset('image/1.jpg')}}" alt="" class="img-fluid">
@@ -413,6 +435,9 @@ background-color:white;
             <img src="{{asset('image/2.jpg')}}" alt="" class="img-fluid">
         </div>
     </div>
+
+</div>
+    
 </div>
 
 
@@ -466,13 +491,18 @@ background-color:white;
   
 <div class="container container-sm mt-5 py-3">
 <div class="row">
-
-    <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 mt-2">
+<div data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine">
+      <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 mt-2">
     <h1 class="mb-3" style="margin-top: 100px">Get in touch</h1>
         <h6>Tell us about your projects, we are excited <br> to hear from you</h6>
       </div>
-
-    <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 div3">
+</div>
+   
+<div data-aos="fade-up"
+     data-aos-duration="3000">
+     <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 div3">
         <form action="" class="container mb-5 mt-4">
             <label class="font-weight-bold text-dark">Full Name:</label>
             <input type="text" placeholder="Full Name..." class="form-control">
@@ -487,6 +517,8 @@ background-color:white;
         </form>
 
     </div>
+</div>
+    
 
 </div>
 </div>
@@ -544,5 +576,8 @@ background-color:white;
                         </div>
                         </div>
                         </div>
+                        <script>
+  AOS.init();
+</script>
         </body>
     </html>
