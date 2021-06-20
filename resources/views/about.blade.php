f569d32bfa9c791709ee5d2a5b63b962aa87923c
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-06-20 04:08:00 -0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -212,10 +212,13 @@ This is why we created GAINS and this is what we are fighting for everyday.</p>
          <div class="card">
     <img class="card-img-top img-fluid" src="{{asset('image/g3.jpg')}}" alt="Card image">
     <div class="card-body">
-    <img class="img-fluid text-center rounded-circle mt-n5" src="{{asset('image/1.jpg')}}" alt="Card image" height="100px" width="100px">
+    <div class="d-flex justify-content-center align-items-center">
+    <img class="img-fluid text-center rounded-circle mt-n5" src="{{asset('image/1.jpg')}}" alt="Card image" height="120px" width="120px">
       <h4 class="card-title text-center">Alexandre Raffin</h4>
       <h6 class="card-text text-center">CEO & Co-Founder</h6>
        <i class="fab fa-linkedin-in text-center text-success fa-2x"></i>
+       </div>
+   
        <hr>
       <a href="#" class="btn btn-outline-primary text-center">View Bio</a>
     </div>
