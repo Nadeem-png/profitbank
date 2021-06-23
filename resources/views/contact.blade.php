3b6e50e618dadac1502dbf77c277e5a4b8ab9298
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-06-23 00:04:04 -0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -96,22 +96,22 @@
 </div>
 
 
-<div class="container container-sm">
-<div class="row">
+<div class="container container-sm mt-5 py-3">
 <div data-aos="fade-up"
      data-aos-duration="3000">
-      <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 mt-2 ">
+     <div class="row">
+      <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 mt-2">
     <h1 class="mb-3" style="margin-top: 100px">Get in touch</h1>
         <h6>Tell us about your projects, we are excited <br> to hear from you</h6>
       </div>
-
-    <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 div3">
+   
+     <div class="col-lg-6 col-sm-12 col-xs-12 col-md-6 div3">
         <form action="" class="container mb-5 mt-4">
             <label class="font-weight-bold text-dark">Full Name:</label>
-            <input type="text" placeholder="Full Name..." class="form-control mb-2">
+            <input type="text" placeholder="Full Name..." class="form-control">
 
             <label class="text-dark font-weight-bold">Email:</label>
-            <input type="email" placeholder="Email..." class="form-control mb-2">
+            <input type="email" placeholder="Email..." class="form-control">
 
             <label class="font-weight-bold text-dark">Your Message:</label>
             <textarea type="text" placeholder="Your Message" class="form-control" rows="5" style="resize: none"></textarea>
@@ -119,10 +119,10 @@
             <a href="" class="btn  mt-3 py-2 px-4 btn-primary ">Send Massege</a>
         </form>
 
-    </div>
 
 </div>
-   
+    
+
 </div>
 </div>
 <hr>
